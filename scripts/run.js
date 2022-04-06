22d849a137d69261ac7411d154d7c3707a775c0b
Aleksey Lisun <insuline.ru@gmail.com>
2022-04-07 00:48:41 +0300

    ✨ feat: add register method and records and domains mappings

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,20 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
-};
+  console.log("Contract deployed by:", owner.address);
+
+  const txn = await domainContract.register("doom");
+  await txn.wait();
+
+  const domainOwner = await domainContract.getAddress("doom");
+  console.log("Owner of domain doom:", domainOwner);
+
+  txn = await domainContract.connect(randomPerson).setRecord("doom", "It's russian scam! AHAHA");
+  await txn.wait();
+}
 
 const runMain = async () => {
   try {
